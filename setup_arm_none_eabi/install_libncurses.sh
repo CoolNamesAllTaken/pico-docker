ec33f744dacd1a57418a2362f80fef3916de289a
John McNelly <jkailimcnelly@gmail.com>
2021-12-22 12:24:39 -0800

    Add setups scripts for arm-none-eabi

---
+++ setup_arm_none_eabi/install_libncurses.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install libncurses-dev
+sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5
+sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/x86_64-linux-gnu/libtinfo.so.o5
