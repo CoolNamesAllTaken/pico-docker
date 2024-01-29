5d7d5426c41bcca6e7640411fc4b1ae20b87ae63
John McNelly <jkailimcnelly@gmail.com>
2024-01-28 18:25:09 -0800

    Fix issues with jlink and arm-none-eabi install

--- setup_scripts/setup_arm_none_eabi/install_arm_none_eabi.sh
+++ setup_scripts/setup_arm_none_eabi/install_arm_none_eabi.sh
@@ -7,13 +7,24 @@ sudo apt remove gcc-arm-none-eabi
 
 mkdir temp
 wget -O temp/${latest_arm_none_eabi}.tar.bz2 $latest_arm_none_eabi_url
-sudo tar xjf temp/${latest_arm_none_eabi}.tar.bz2 -C /usr/share/
-sudo ln -s /usr/share/${latest_arm_none_eabi}/bin/arm-none-eabi-gcc /usr/bin/arm-none-eabi-gcc
-sudo ln -s /usr/share/${latest_arm_none_eabi}/bin/arm-none-eabi-g++ /usr/bin/arm-none-eabi-g++
-sudo ln -s /usr/share/${latest_arm_none_eabi}/bin/arm-none-eabi-gdb /usr/bin/arm-none-eabi-gdb
-sudo ln -s /usr/share/${latest_arm_none_eabi}/bin/arm-none-eabi-size /usr/bin/arm-none-eabi-size
-sudo ln -s /usr/share/${latest_arm_none_eabi}/bin/arm-none-eabi-objcopy /usr/bin/arm-none-eabi-objcopy
-sudo ln -s /usr/share/${latest_arm_none_eabi}/bin/arm-none-eabi-objdump /usr/bin/arm-none-eabi-objdump
+
+# Explicitly create the destination directory.
+sudo mkdir /usr/local/$latest_arm_none_eabi
+
+# Make sure bzip2 exists so that tar doesn't crash!
+sudo apt install -y bzip2
+
+# Un-tar the files into a versioned arm-none-eabi folder in /usr/local.
+# --strip-components 1 option removes the top level directory
+sudo tar xjfv temp/${latest_arm_none_eabi}.tar.bz2 -C /usr/local/$latest_arm_none_eabi --strip-components 1
+
+# Creat symlinks in /usr/bin
+sudo ln -s /usr/local/${latest_arm_none_eabi}/bin/arm-none-eabi-gcc /usr/bin/arm-none-eabi-gcc
+sudo ln -s /usr/local/${latest_arm_none_eabi}/bin/arm-none-eabi-g++ /usr/bin/arm-none-eabi-g++
+sudo ln -s /usr/local/${latest_arm_none_eabi}/bin/arm-none-eabi-gdb /usr/bin/arm-none-eabi-gdb
+sudo ln -s /usr/local/${latest_arm_none_eabi}/bin/arm-none-eabi-size /usr/bin/arm-none-eabi-size
+sudo ln -s /usr/local/${latest_arm_none_eabi}/bin/arm-none-eabi-objcopy /usr/bin/arm-none-eabi-objcopy
+sudo ln -s /usr/local/${latest_arm_none_eabi}/bin/arm-none-eabi-objdump /usr/bin/arm-none-eabi-objdump
 # Add other required commands here!
 
 rm -rf temp
--- setup_scripts/setup_jlink/install_jlink.sh
+++ setup_scripts/setup_jlink/install_jlink.sh
@@ -7,11 +7,12 @@ mkdir temp
 
 wget -O temp/jlink_linux.deb --post-data 'accept_license_agreement=accepted&non_emb_ctr=confirmed&submit=Download+software' $jlink_linux_url
 
-# Create a fake udevadm so the postinstall doesn't fail (Docker image doesn't have udev).
+# Remove the post-install script from the jlink package to avoid encountering an issue with udevadm not existing on the Docker container.
 # Fix provided here: https://forum.segger.com/index.php/Thread/8953-SOLVED-J-Link-Linux-installer-fails-for-Docker-containers-Error-Failed-to-update/
-RUN echo '#!/bin/bash\necho not running udevadm "$@"' > /usr/bin/udevadm && chmod +x /usr/bin/udevadm \
-    && [...] && dpkg -i temp/jlink_linux.deb
-
+dpkg --unpack temp/jlink_linux.deb
+rm -f /var/lib/dpkg/info/jlink.postinst
+dpkg --configure jlink
+apt install -yf
 # Note that dpkg installs stuff into opt/SEGGER.
 
 rm -rf temp
